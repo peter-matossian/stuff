5169b0c35106bfce9720ccfc25045b2737cbab92
peter-matossian <peter7898m@hotmail.com>
2016-01-26 22:08:17 +0000

    jsStuff

---
+++ Warp.js
@@ -0,0 +1,13 @@
+﻿var warptarget : Transform;
+
+function Update () {
+
+}
+
+function OnTriggerEnter (col : Collider) {
+
+	if (col.gameObject.tag == "Warp001"){
+	this.transform.position = warptarget.position;
+	}
+
+}
